8bb34ac0e89a7aa481a199345a4fcbaa2b6e33c9
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2019-12-15 10:42:34 +0100

    Stage (#9)
    * Initial beacon implementation (#1)
    
    * Initial implementation of a widget. It allows for two methods of fetching CV data, via button or automatically on page load.
    
    * Added integration guidelines to readme.
    
    * Correction of readme typo
    
    * Further changes to beacon github readme
    
    * Fix table markup in readme
    
    * Af connect module (#3)
    
    * Simplified the module functionality and preparation for CV polling.
    
    * Update README.md
    
    * Update jobtechdev.md
    
    * Feature/polling (#4)
    
    * Simplified the module functionality and preparation for CV polling.
    
    * Update README.md
    
    * Update jobtechdev.md
    
    * WIP: Implementing polling, currently using mocked response
    
    * End-to-end success
    
    * Api key requirement (#6)
    
    * Simplified the module functionality and preparation for CV polling.
    
    * Update README.md
    
    * Update jobtechdev.md
    
    * WIP: Implementing polling, currently using mocked response
    
    * End-to-end success
    
    * Api key configuration required
    
    * Clean up (#7)
    
    * Added tests for module functions: fetchSession and fetchCV
    
    * Cleaned up code
    
    * Improved the README guidelines, some parts still needs to be filled in.
    
    * Improved README
    
    * Update README.md
    
    * Updated Readme and config defaults
    
    * updated readme
    
    * Changed defaults
    
    * Updated readme

---
+++ __test__/module.test.js
@@ -0,0 +1,47 @@
+const mockAxios = require("axios");
+
+test("getSession with mock api", () => {
+  mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve({ token: "aa31667d-afde-4941-8749-d71ff90bc247" })
+  );
+
+  const config = {
+    afPortabilityUrl: "https://portabilitystage.arbetsformedlingen.se"
+  };
+
+  const connectModule = require("../lib/connect-module");
+  return connectModule.getSession(config).then(token => {
+    expect(token).toBeDefined();
+    expect(typeof token).toBe("string");
+    expect(token).toBe("aa31667d-afde-4941-8749-d71ff90bc247");
+  });
+});
+
+test("getEnvelope with mock api", () => {
+  mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve(require("./cv.json"))
+  );
+
+  const onResponse = data => {
+    console.log("Response: ", data);
+  };
+
+  const config = {
+    pollRate: "1000", // 1 second
+    timeout: "300000", // 5 minutes
+    afConnectUrl: "https://demotest.arbetsformedlingen.se",
+    afPortabilityUrl: "https://portabilitystage.arbetsformedlingen.se",
+    onResponse: onResponse
+  };
+
+  const connectModule = require("../lib/connect-module");
+  return connectModule
+    .getEnvelope(config, "aa31667d-afde-4941-8749-d71ff90bc247")
+    .then(cv => {
+      expect(cv).toBeDefined();
+      expect(typeof cv).toBe("object");
+      expect(cv.documentId).toBeDefined();
+      expect(cv.person).toBeDefined();
+      expect(cv.profiles).toBeDefined();
+    });
+});
